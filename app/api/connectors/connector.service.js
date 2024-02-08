6678d04e4162f88f21a73f82cc20133dcfd17c55
Rafael Costa <44804@tjmt.jus.br>
2024-02-07 20:00:24 -0400

    feat: modified connectors service

--- app/api/connectors/connector.service.js
+++ app/api/connectors/connector.service.js
@@ -1,11 +1,22 @@
 import {Connector} from "../../shared/models/Connector.js";
 
-
+/**
+ * Get all connectors
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
 export const getAllConnectors = async (req, res) => {
     const connectors = await Connector.findAll();
     res.status(200).json(connectors);
 }
 
+/**
+ * Get connector by id
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
 export const getConnectorById = async (req, res) => {
     const id = req.params.id
     const getConnector = async () => {
@@ -20,5 +31,78 @@ export const getConnectorById = async (req, res) => {
         await getConnector();
     } catch (error) {
         res.status(404).json({message: "Connector not found", error: error})
+    }
+}
+
+/**
+ * Register connector
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+export const createConnector = async (req, res) => {
+    try {
+        const connector = await Connector.create({
+            connectorType: req.body.connectorType,
+            connPower: req.body.connPower,
+            available: req.body.available
+        })
+        res.status(201).json({message: "Connector saved successfully", connector: connector})
+    } catch (error) {
+        res.status(400).json({message: "Failed to save connector", error: error})
+    }
+}
+
+/**
+ * Update connector data
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+export const updateConnector = async (req, res) => {
+    const connectorId = req.params.id;
+    const connectorType = req.body.connectorType;
+    const connPower = req.body.connPower;
+    const available = req.body.available;
+    const updateConnectorData = async () => {
+        const connector = await Connector.findByPk(connectorId);
+        if (connector !== null) {
+            connector.connectorType = connectorType;
+            connector.connPower = connPower;
+            connector.available = available;
+            await connector.save();
+            res.status(200).json({message: "Connector updated successfully", connector: connector});
+        } else {
+            throw new Error();
+        }
+    }
+    try {
+        await updateConnectorData();
+    } catch (error) {
+        res.status(404).json({message: "Connector not found", error: error});
+    }
+}
+
+/**
+ * Delete connector by id
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+export const deleteConnector = async (req, res) => {
+    const id = req.params.id;
+    const deleteConnector = async () => {
+        const connector = await Connector.findByPk(id);
+        if (connector !== null) {
+            await connector.destroy();
+            res.status(200).json({message: "Connector deleted successfully"});
+        } else {
+            throw new Error();
+        }
+    }
+    try {
+        await deleteConnector();
+    } catch (error) {
+        res.status(404).json({message: "Connector not found", error: error});
     }
 }
