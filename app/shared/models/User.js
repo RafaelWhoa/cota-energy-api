28f1f527202758d5b0693deaae0a15653a19637f
Rafael Costa <44804@tjmt.jus.br>
2024-02-28 10:40:37 -0400

    feat: created User model

---
+++ app/shared/models/User.js
@@ -0,0 +1,26 @@
+import {sequelize} from "../db/db.connection.js";
+import {DataTypes} from "sequelize";
+
+export const User = sequelize.define("users", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
