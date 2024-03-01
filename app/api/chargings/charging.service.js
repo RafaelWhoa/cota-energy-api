38114da8f94aaf1653e7fb90e66d0d084bf59192
Rafael Costa <44804@tjmt.jus.br>
2024-03-01 13:55:59 -0400

    feat: created method to update charging data

--- app/api/chargings/charging.service.js
+++ app/api/chargings/charging.service.js
@@ -8,3 +8,32 @@ export const startCharging = async (req, res) => {
     })
     res.status(201).json({message: "Charging saved successfully", charging: charging});
 }
+
+export const updateCharging = async (req, res) => {
+    const chargingId = req.params.id;
+    const totalPower = req.body.total_power;
+    const currentPower = req.body.current_power;
+    const chargingPercentage = req.body.charging_percentage;
+
+    const updateChargingData = async () => {
+        const charging = await Charging.findByPk(chargingId);
+        if (charging !== undefined) {
+            await charging.update({
+                total_power: totalPower,
+                current_power: currentPower,
+                charging_percentage: chargingPercentage,
+            })
+            await charging.save();
+        }
+        else {
+            res.status(404).json({message: "Charging not found"});
+        }
+        try {
+            updateChargingData().then(() => {
+                res.status(200).json({message: "Charging updated successfully", charging: charging});
+            })
+        } catch (error) {
+            res.status(400).json({message: "Failed to update charging", error: error.message});
+        }
+    }
+}
